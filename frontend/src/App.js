d07afbea8c72f2cf5085dd3ce8cf7a4f4be7643a
SethKalkstein <sethkal7@yahoo.com>
2021-03-06 12:45:17 -0500

    created header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to the Faux Shop</h1>
+    <Header />
+      <main>
+        <h1>Welcome to the Faux Shop</h1>
+      </main>
+      <Footer />
     </>   
   );
 }
